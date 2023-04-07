daabe3c010acdaa4d82f23d633e8ffd5edb70862
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-04-07 08:34:42 +0800

    3c

--- README.md
+++ README.md
@@ -1,25 +1,24 @@
-# NiceGUI-ChatGPT3.5-Render
-# 一個使用NiceGUI框架和GPT3.5 turbo模型官方API，快速建置Web app於平台Render。
+# Flet-ChatGPT3.5-Render
+# 一個使用Flet框架和GPT3.5 turbo模型官方API，快速建置Web app於平台Render。
 
 <div align="center">
-  <img src="demo/nicegui1.png" width="600"/>
+  <img src="demo/flet1.png" width="600"/>
+</d
+<div align="center">
+  <img src="demo/flet2.png" width="600"/>
 </div>
 
-<div align="center">
-  <img src="demo/nicegui2.png" width="600"/>
-</div>
-
-### [English](https://github.com/pyfbsdk59/NiceGUI-ChatGPT3.5-Render/blob/main/README_en.md)
-### [日本語](https://github.com/pyfbsdk59/NiceGUI-ChatGPT3.5-Render/blob/main/README_jp.md)
+### [English](https://github.com/pyfbsdk59/Flet-ChatGPT3.5-Render/blob/main/README_en.md)
+### [日本語](https://github.com/pyfbsdk59/Flet-ChatGPT3.5-Render/blob/main/README_jp.md)
 
 
-### 1. 本專案參考了以下前輩和官方的方案改成製作，只針對剛學習Python或nicegui的朋友來佈置Web app在Render上。Render可取代取消免費方案的Heroku，來測試Side Project。
+### 1. 本專案參考了以下前輩和官方的方案改成製作，只針對剛學習Python或Flet的朋友來佈置Web app在Render上。Render可取代取消免費方案的Heroku，來測試Side Project。
 
-https://github.com/joeychrys/streamlit-chatGPT/<br><br>
-https://nicegui.io/<br><br>
+https://flet.dev/docs/guides/python/deploying-web-app/hosting-providers/self-hosting<br><br>
+https://github.com/flet-dev/examples/tree/main/python/apps<br><br>
 
 <div align="center">
-  <img src="demo/niceweb1.png" width="600"/>
+  <img src="demo/flet_web01.png" width="600"/>
 </div>
 
 #### 2. 本專案部屬在Render平台上。Render網站中，選擇新增「Web Services」，可用github帳號匯入此專案，可先fork到自己的帳號，然後設定自己的名稱和選擇免費free方案。不需設定環境變數。
--- src/main_backup.py
+++ src/main_backup.py
@@ -1,6 +1,6 @@
 import flet as ft
 import os, openai
-##os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 conversation = []
@@ -9,15 +9,12 @@ class ChatGPT:
     
 
     def __init__(self):
-        self.api_key = ""
         self.messages = conversation
         self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
 
-    def save_api_key(self, user_input0):
-        self.api_key = user_input0
+
 
     def get_response(self, user_input):
-        openai.api_key = self.api_key
         conversation.append({"role": "user", "content": user_input})
         
 
@@ -43,21 +40,6 @@ chatgpt = ChatGPT()
 
 
 def main(page: ft.Page):
-    
-    def btn_click0(e):
-        if not api_key.value:
-            api_key.error_text = "Please enter your api key"
-            page.update()
-        else:
-            name0 = api_key.value
-            chatgpt.save_api_key(name0)
-            #page.clean()
-            page.add(ft.Text(f"openai api key saved!"))    
-
-
-    api_key = ft.TextField(label="Your openai api key")
-        
-        
     def btn_click(e):
         if not txt_name.value:
             txt_name.error_text = "Please enter your prompt"
@@ -70,7 +52,7 @@ def main(page: ft.Page):
 
     txt_name = ft.TextField(label="Your prompt")
 
-    page.add(api_key, ft.ElevatedButton("Save api key!", on_click=btn_click0), txt_name, ft.ElevatedButton("Generate!", on_click=btn_click))
+    page.add(txt_name, ft.ElevatedButton("Generate!", on_click=btn_click))
 
 
 ft.app(target=main, view=None, port=8502)
